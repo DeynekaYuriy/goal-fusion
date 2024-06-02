ce7f92e98bf4840623ce3e8dd6d4e7a13140102b
Yuriy Deyneka <ipz213_dyum@student.ztu.edu.ua>
2024-06-02 20:18:22 +0300

    Add endpoints for profile and update profile

---
+++ src/pages/api/profile/index.ts
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { connectToDatabase } from "../../../utils/mongodb";
+import { authMiddleware, AuthenticatedRequest } from "../../../middleware/auth";
+import { ObjectId } from "mongodb";
+
+const handler = async (req: AuthenticatedRequest, res: NextApiResponse) => {
+  if (req.method === "GET") {
+    const { db } = await connectToDatabase();
+
+    const user = await db.collection("users").findOne({ _id: new ObjectId(req.user.id) });
+    
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    return res
+      .status(200)
+      .json({ email: user.email, name: user.name, age: user.age });
+  } else {
+    res.status(405).json({ message: "Method Not Allowed" });
+  }
+};
+
+export default authMiddleware(handler);
