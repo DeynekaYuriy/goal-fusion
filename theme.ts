c7dac39f61c29ede7f603405bc6e2f7f332f7ae6
Yuriy Deyneka <ipz213_dyum@student.ztu.edu.ua>
2024-06-03 19:55:11 +0300

    Add Storybook

--- theme.ts
+++ theme.ts
@@ -29,7 +29,7 @@ const theme = {
     },
     secondary: {
       bg: "gray",
-      color: "black",
+      color: "white",
       padding: "12px 24px",
       borderRadius: "8px",
       boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
